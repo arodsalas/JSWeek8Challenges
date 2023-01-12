a4745537b2761e0e9147c5bb65f912147ef8ebfb
arodsalas <rodriguezsalas2001@gmail.com>
2023-01-12 13:22:50 -0500

    Medium Javascript Week 8 Challenges

---
+++ medium/script.js
@@ -0,0 +1,5 @@
+var currentMonth = ("1")
+var months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", 
+              "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
+
+console.log(months[currentMonth - 1]);
