ef760e614c1644f5b292c210212194a6032540ba
arodsalas <rodriguezsalas2001@gmail.com>
2023-01-12 13:31:24 -0500

    easy Javascript Week 8 Challenges

---
+++ easy/script.js
@@ -0,0 +1,8 @@
+const students = [
+    { lname: "Rick", fname: "Bob", age: 15 },
+    { lname: "Rodriguez", fname: "Bryan", age: 20 },
+    { lname: "James", fname: "Rick", age: 23 },
+];
+
+console.log(`Hello, my name is ${students[1].fname} ${students[1].lname} and I'm ${students[1].age} years old`
+);
